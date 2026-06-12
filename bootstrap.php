db4bbd89f2dde0e2951a0e337b0bd41fa4bba1e2
Daniil Andrushko <dan007sar@gmail.com>
2026-06-12 12:38:04 +0200

    minor fix

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,12 @@ use Nette\Bootstrap\Configurator;
 require __DIR__ . '/vendor/autoload.php';
 
 $configurator = new Configurator();
+
 $configurator->setTempDirectory(__DIR__ . '/temp');
+
+$configurator->enableTracy(__DIR__ . '/log');
+
+$configurator->setDebugMode(true);
 
 $configurator->createRobotLoader()
     ->addDirectory(__DIR__ . '/app')
