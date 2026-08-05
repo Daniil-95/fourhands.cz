b8a429a3d0075342965470b7158e4d14e9cd1747
Daniil Andrushko <dan007sar@gmail.com>
2026-08-05 16:05:34 +0200

    Přidání konfigurace pro prostředí www a úprava režimu ladění

--- bootstrap.php
+++ bootstrap.php
@@ -10,13 +10,33 @@ $configurator->setTempDirectory(__DIR__ . '/temp');
 
 $configurator->enableTracy(__DIR__ . '/log');
 
-$configurator->setDebugMode(true);
+/**
+ * Debug mode
+ */
+$configurator->setDebugMode(!isset($_SERVER['HTTP_HOST']) || $_SERVER['HTTP_HOST'] !== 'fourhands.cz');
 
+/**
+ * RobotLoader
+ */
 $configurator->createRobotLoader()
     ->addDirectory(__DIR__ . '/app')
     ->register();
 
+/**
+ * Common configuration
+ */
 $configurator->addConfig(__DIR__ . '/app/config/common.neon');
-$configurator->addConfig(__DIR__ . '/app/config/local.neon');
+
+/**
+ * Environment configuration
+ */
+if (
+    isset($_SERVER['HTTP_HOST'])
+    && in_array($_SERVER['HTTP_HOST'], ['fourhands.cz', 'www.fourhands.cz'], true)
+) {
+    $configurator->addConfig(__DIR__ . '/app/config/www.neon');
+} else {
+    $configurator->addConfig(__DIR__ . '/app/config/local.neon');
+}
 
 return $configurator->createContainer();
