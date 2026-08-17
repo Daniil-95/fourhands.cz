589db828d47572f89dbf093078cc31f332ad6fa7
Daniil Andrushko <dan007sar@gmail.com>
2026-08-18 00:59:08 +0200

    Přidání rychlých akcí na řídicí panel a vylepšení stylů pro administraci

--- app/AdminModule/presenters/DashboardPresenter.php
+++ app/AdminModule/presenters/DashboardPresenter.php
@@ -16,5 +16,49 @@ final class DashboardPresenter extends BaseAdminPresenter
     public function renderDefault(): void
     {
         $this->template->pages = PageSectionRepository::PAGES;
+        $this->template->quickActions = [
+            [
+                'title' => 'Události',
+                'description' => 'Správa koncertů a termínů.',
+                'href' => ':Admin:Event:default',
+            ],
+            [
+                'title' => 'Z pódia',
+                'description' => 'Publikace, články a zmínky.',
+                'href' => ':Admin:Publication:default',
+            ],
+            [
+                'title' => 'Fotogalerie',
+                'description' => 'Fotografie a jejich pořadí.',
+                'href' => ':Admin:Media:default',
+                'params' => ['type' => 'photo'],
+            ],
+            [
+                'title' => 'Videogalerie',
+                'description' => 'YouTube videa a náhledy.',
+                'href' => ':Admin:Media:default',
+                'params' => ['type' => 'video'],
+            ],
+            [
+                'title' => 'Zprávy',
+                'description' => 'Zprávy z kontaktního formuláře.',
+                'href' => ':Admin:Inquiry:default',
+            ],
+            [
+                'title' => 'Navigační menu',
+                'description' => 'Položky menu pro web.',
+                'href' => ':Admin:Navigation:default',
+            ],
+            [
+                'title' => 'Nastavení webu',
+                'description' => 'Kontakty, odkazy a obecná nastavení.',
+                'href' => ':Admin:Setting:default',
+            ],
+            [
+                'title' => 'SEO',
+                'description' => 'Metadata a náhledy při sdílení.',
+                'href' => ':Admin:Setting:seo',
+            ],
+        ];
     }
 }
