44be99448a65d1a3f83b11f0e2bfb4dcf1105a2e
Daniil Andrushko <dan007sar@gmail.com>
2026-08-18 02:51:43 +0200

    Změna názvu sekce na "Umělkyně" a aktualizace popisu videí na "Videogalerie" pro českou verzi a "Video gallery" pro anglickou verzi.

---
+++ db/032_fix_homepage_video_label.sql
@@ -0,0 +1,7 @@
+UPDATE page_sections
+SET subtitle = 'Videogalerie'
+WHERE page_key = 'homepage' AND section_key = 'videos' AND lang = 'cs';
+
+UPDATE page_sections
+SET subtitle = 'Video gallery'
+WHERE page_key = 'homepage' AND section_key = 'videos' AND lang = 'en';
